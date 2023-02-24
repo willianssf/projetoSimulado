0d92c0390fb061a2ea0f7d9db0816505c426406a
willian <wsfranca@fazenda.ms.gov.br>
2023-02-24 08:31:21 -0400

    Tela de login criada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {SessionUserComponent} from "./components/session-user/session-user.component";
+import {LoginComponent} from "./components/login/login.component";
 
 const routes: Routes = [
-  {path: '', component: SessionUserComponent}
+  {path: '', component: SessionUserComponent},
+  {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {MatCardModule} from '@angular/material/card';
-import {MatGridListModule} from '@angular/material/grid-list';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,13 +9,15 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SessionUserComponent } from './components/session-user/session-user.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    SessionUserComponent
+    SessionUserComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
